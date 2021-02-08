80dc9c1b1b9e76fe27f39e9d3131d20ec57b9a92
Vitaliy <72467257+imposter18@users.noreply.github.com>
2021-02-08 23:29:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+" use strict ";
+
+// alert('1');
+
+// const result = confirm('Are you here');
+// console.log(result)
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log( +answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '' );
+// answers[1] = prompt('Как ваше фамилия?', '' );
+// answers[2] = prompt('Сколько вам лет?', '' );
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
